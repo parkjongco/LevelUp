d19be24e5fcaaf5069d13e9a6850a4bfff968c03
JongHo <ehdus232323@naver.com>
2024-10-07 18:05:15 +0900

    gitignore

---
+++ LevelUpServer/src/test/java/com/levelUp/LevelUpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levelUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelUpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
