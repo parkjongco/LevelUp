2ba81a7a2df20d532a6753f5ca5d399085ab606f
Minseo <wan7078@naver.com>
2024-10-21 22:56:40 +0900

    axios 코드 추가
     axios 코드 추가

--- levelupfront/src/api/api.js
+++ levelupfront/src/api/api.js
@@ -1,5 +1,7 @@
 /**
  *  API 사용 함수 작성
  *  axios 데이터 통신 관리 폴더
- *  해당기능.js 파일 만들어서 관리 
+ *  해당기능.js 파일 만들어서 관리
  */
+
+import { api, host } from '../config/config'
--- levelupfront/src/config/config.js
+++ levelupfront/src/config/config.js
@@ -1,5 +1,10 @@
-/** 설정 변수 또는 함수 만들어서 보관 **/
-const { REACT_APP_BASE_URL } = process.env
+import axios from 'axios'
 
-// API base url
+// 설정 변수
+const { REACT_APP_BASE_URL } = process.env
 export const host = REACT_APP_BASE_URL
+
+// API base url 설정
+export const api = axios.create({
+    baseURL: `${host}`,
+})
